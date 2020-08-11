1f32d546171a5a5be942aefc1d66e42c7a475408
Sharique Ahmad <shriqueumar@gmail.com>
2020-08-11 14:06:48 +0530

    Prizes added to HowToPlay

---
+++ gcc_mobile/build/precache-manifest.335a519d1c0aea9c4086fc5d30087b45.js
@@ -0,0 +1,462 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47873d905c764848775d327981b8fe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e15a7ce2d38ea54ffc70",
+    "url": "/static/css/12.8921c4c4.chunk.css"
+  },
+  {
+    "revision": "ee48ebdd846daebd75cd",
+    "url": "/static/css/main.d01988b7.chunk.css"
+  },
+  {
+    "revision": "9d5c00676090d3a99d92",
+    "url": "/static/js/0.ecb9ae7e.chunk.js"
+  },
+  {
+    "revision": "96f94c8998de4af73404",
+    "url": "/static/js/1.61500337.chunk.js"
+  },
+  {
+    "revision": "f5831fa7fa2dc031234f",
+    "url": "/static/js/100.c46b29d4.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/static/js/100.c46b29d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab7c6e03802b506fdb9",
+    "url": "/static/js/101.01f817d4.chunk.js"
+  },
+  {
+    "revision": "e8d6d4cfa6414df93e27",
+    "url": "/static/js/102.e657028e.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/static/js/102.e657028e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15a7ce2d38ea54ffc70",
+    "url": "/static/js/12.fcf6bda1.chunk.js"
+  },
+  {
+    "revision": "40dcb05f363ef4b3a753472118fbbf53",
+    "url": "/static/js/12.fcf6bda1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61161883019a03eddbe4",
+    "url": "/static/js/13.bf2a3754.chunk.js"
+  },
+  {
+    "revision": "3dfe4b5ea1d62e35e7b6",
+    "url": "/static/js/14.ddd19372.chunk.js"
+  },
+  {
+    "revision": "fb785c1910286899f9e5",
+    "url": "/static/js/15.ac13373f.chunk.js"
+  },
+  {
+    "revision": "56292538e678613a2518",
+    "url": "/static/js/16.75f48d0b.chunk.js"
+  },
+  {
+    "revision": "a0be01e01f821c3c348b",
+    "url": "/static/js/17.77a9e4fc.chunk.js"
+  },
+  {
+    "revision": "5fd4774e145968621f0a",
+    "url": "/static/js/18.359b5347.chunk.js"
+  },
+  {
+    "revision": "dd3982a22dd36e5fc1f1",
+    "url": "/static/js/19.af3afa95.chunk.js"
+  },
+  {
+    "revision": "470d7d6442e6f29302c4",
+    "url": "/static/js/2.8dcf3565.chunk.js"
+  },
+  {
+    "revision": "099ad134678233488868",
+    "url": "/static/js/20.b0e2beb9.chunk.js"
+  },
+  {
+    "revision": "233bc8feeea12948d335",
+    "url": "/static/js/21.b76ee8b8.chunk.js"
+  },
+  {
+    "revision": "66b2b63b6287fab42f78",
+    "url": "/static/js/22.7c0279cc.chunk.js"
+  },
+  {
+    "revision": "0b869ff00b7c0c4f7466",
+    "url": "/static/js/23.b05ad74e.chunk.js"
+  },
+  {
+    "revision": "a852cfc22bcfc965d7a2",
+    "url": "/static/js/24.ec491338.chunk.js"
+  },
+  {
+    "revision": "8c01559ace651936525b",
+    "url": "/static/js/25.6b65cb77.chunk.js"
+  },
+  {
+    "revision": "bd5b2cd86b25cf6b9e40",
+    "url": "/static/js/26.45fdc323.chunk.js"
+  },
+  {
+    "revision": "d8326e3e0f964357c87d",
+    "url": "/static/js/27.50bcc7fe.chunk.js"
+  },
+  {
+    "revision": "f42c071da4c3d976c6c4",
+    "url": "/static/js/28.9c034dde.chunk.js"
+  },
+  {
+    "revision": "5fa75778ccb6a5721573",
+    "url": "/static/js/29.1cb5866e.chunk.js"
+  },
+  {
+    "revision": "86eee03c5de8d417b91b",
+    "url": "/static/js/3.66a2cba6.chunk.js"
+  },
+  {
+    "revision": "2c71ac658ed7b3e364cb",
+    "url": "/static/js/30.ef5d5413.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/static/js/30.ef5d5413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25fd12219305c63a5771",
+    "url": "/static/js/31.3f693510.chunk.js"
+  },
+  {
+    "revision": "309d0e51c9cfdc561bd6",
+    "url": "/static/js/32.8c473667.chunk.js"
+  },
+  {
+    "revision": "58e741d682f2e80e63ef",
+    "url": "/static/js/33.b77dd756.chunk.js"
+  },
+  {
+    "revision": "ca434e89865b0a0839bb",
+    "url": "/static/js/34.209fcf07.chunk.js"
+  },
+  {
+    "revision": "8a4c4f6990e5ad7aeb73",
+    "url": "/static/js/35.0a46ea47.chunk.js"
+  },
+  {
+    "revision": "83eb5d4004c83f41e30f",
+    "url": "/static/js/36.793e393f.chunk.js"
+  },
+  {
+    "revision": "825c4a047c784b926f2c",
+    "url": "/static/js/37.62ac0528.chunk.js"
+  },
+  {
+    "revision": "d17106f316e5855ccfec",
+    "url": "/static/js/38.e9f75575.chunk.js"
+  },
+  {
+    "revision": "e3c640cf190d017d2b92",
+    "url": "/static/js/39.a4a5ff79.chunk.js"
+  },
+  {
+    "revision": "446b5a752baad3155865",
+    "url": "/static/js/4.62e2916b.chunk.js"
+  },
+  {
+    "revision": "0cce9c154eb7ce11262a",
+    "url": "/static/js/40.ac1b782e.chunk.js"
+  },
+  {
+    "revision": "50543fbe9660bb96f58e",
+    "url": "/static/js/41.835f3a89.chunk.js"
+  },
+  {
+    "revision": "bcdb1af7fea7249fedf1",
+    "url": "/static/js/42.dab2e875.chunk.js"
+  },
+  {
+    "revision": "f24bb3e56f99cbb07e89",
+    "url": "/static/js/43.089ee202.chunk.js"
+  },
+  {
+    "revision": "131e15b817f695128f18",
+    "url": "/static/js/44.0797d701.chunk.js"
+  },
+  {
+    "revision": "0bc6f6c4a63c5c3013a6",
+    "url": "/static/js/45.e339bd80.chunk.js"
+  },
+  {
+    "revision": "4a73920c47c192e51f17",
+    "url": "/static/js/46.90d08cec.chunk.js"
+  },
+  {
+    "revision": "d0ca654bcfee808708d8",
+    "url": "/static/js/47.ff20c069.chunk.js"
+  },
+  {
+    "revision": "2a74363229dcf680003f",
+    "url": "/static/js/48.592a0a78.chunk.js"
+  },
+  {
+    "revision": "4b6fec1a6ab1d35799ed",
+    "url": "/static/js/49.adb0126b.chunk.js"
+  },
+  {
+    "revision": "e88c1f4d20d3a0470366",
+    "url": "/static/js/5.5750c276.chunk.js"
+  },
+  {
+    "revision": "3b0cfb4c11765d4bfb39",
+    "url": "/static/js/50.a9f1f0a1.chunk.js"
+  },
+  {
+    "revision": "eef084d7509ed543fe3c",
+    "url": "/static/js/51.a1d079f7.chunk.js"
+  },
+  {
+    "revision": "2fb0cfb5d67278997f7c",
+    "url": "/static/js/52.abd6b16a.chunk.js"
+  },
+  {
+    "revision": "5f219dd3288849d039a9",
+    "url": "/static/js/53.d49b37ad.chunk.js"
+  },
+  {
+    "revision": "8ffec2924461d8e65159",
+    "url": "/static/js/54.56076724.chunk.js"
+  },
+  {
+    "revision": "a48e65e8ad87f1e1a1da",
+    "url": "/static/js/55.6eb293de.chunk.js"
+  },
+  {
+    "revision": "932bc2fd6c10f9671731",
+    "url": "/static/js/56.8ccf8241.chunk.js"
+  },
+  {
+    "revision": "67c16a5f6ea99339a5c8",
+    "url": "/static/js/57.65e5636a.chunk.js"
+  },
+  {
+    "revision": "90cc00f40321f85da0b5",
+    "url": "/static/js/58.54147bd2.chunk.js"
+  },
+  {
+    "revision": "a3fd66666e33eb50af8a",
+    "url": "/static/js/59.29dce724.chunk.js"
+  },
+  {
+    "revision": "5c13dbc8a454fa3027cb",
+    "url": "/static/js/6.cab9a597.chunk.js"
+  },
+  {
+    "revision": "eea9c8c6bf65170a1f37",
+    "url": "/static/js/60.f75f25a8.chunk.js"
+  },
+  {
+    "revision": "7a574ba2fca4e4ae7726",
+    "url": "/static/js/61.0c7ec8e1.chunk.js"
+  },
+  {
+    "revision": "bc2ab49e1887c135aef9",
+    "url": "/static/js/62.d5590a0d.chunk.js"
+  },
+  {
+    "revision": "8a57e3868052edbdafb0",
+    "url": "/static/js/63.8f92539c.chunk.js"
+  },
+  {
+    "revision": "2f1c40def45739b53d3d",
+    "url": "/static/js/64.394b6988.chunk.js"
+  },
+  {
+    "revision": "a7b9e6170c7701d9a74d",
+    "url": "/static/js/65.69b1fbe3.chunk.js"
+  },
+  {
+    "revision": "edbc8b0351aa2c4c23cc",
+    "url": "/static/js/66.2e4bc659.chunk.js"
+  },
+  {
+    "revision": "25bbe138c01ccc29c771",
+    "url": "/static/js/67.ea2a9863.chunk.js"
+  },
+  {
+    "revision": "77314f8c347037111141",
+    "url": "/static/js/68.eaf0acba.chunk.js"
+  },
+  {
+    "revision": "839afa17342a5c76aa4e",
+    "url": "/static/js/69.16d7f00d.chunk.js"
+  },
+  {
+    "revision": "25b344410a5d590f8821",
+    "url": "/static/js/70.7c88dfca.chunk.js"
+  },
+  {
+    "revision": "0d5b14db1cf8051cd219",
+    "url": "/static/js/71.fedd1252.chunk.js"
+  },
+  {
+    "revision": "f1f58fbc8d754af4301b",
+    "url": "/static/js/72.83b5c729.chunk.js"
+  },
+  {
+    "revision": "ca04b0b9069d674219eb",
+    "url": "/static/js/73.97c39973.chunk.js"
+  },
+  {
+    "revision": "433b9c51aef98d24d5c0",
+    "url": "/static/js/74.980c8842.chunk.js"
+  },
+  {
+    "revision": "1d21327b6dc7385690d2",
+    "url": "/static/js/75.191efa54.chunk.js"
+  },
+  {
+    "revision": "841131ca6edcacd2e985",
+    "url": "/static/js/76.3de5f73a.chunk.js"
+  },
+  {
+    "revision": "3b30d4eb913fc3f0ea75",
+    "url": "/static/js/77.26033d2a.chunk.js"
+  },
+  {
+    "revision": "5d61fbd4e0b900163d12",
+    "url": "/static/js/78.3bd8100c.chunk.js"
+  },
+  {
+    "revision": "4c7be7f1c1139898888e",
+    "url": "/static/js/79.9a58960d.chunk.js"
+  },
+  {
+    "revision": "8039631961b41a3ad060",
+    "url": "/static/js/80.6c90cd6b.chunk.js"
+  },
+  {
+    "revision": "de74ae72ef976fccd662",
+    "url": "/static/js/81.5de145fa.chunk.js"
+  },
+  {
+    "revision": "cf6e551db06d55f91529",
+    "url": "/static/js/82.ee561f96.chunk.js"
+  },
+  {
+    "revision": "6f6e4e92877833820f5c",
+    "url": "/static/js/83.7441b962.chunk.js"
+  },
+  {
+    "revision": "d70c25dff262a8c39bc5",
+    "url": "/static/js/84.313fa40e.chunk.js"
+  },
+  {
+    "revision": "59cd82da55905b13d583",
+    "url": "/static/js/85.9dc1a9e6.chunk.js"
+  },
+  {
+    "revision": "45b3a51e2dbf4be1b856",
+    "url": "/static/js/86.e5f3d3a0.chunk.js"
+  },
+  {
+    "revision": "59ddfad26cd7561c9114",
+    "url": "/static/js/87.774309ec.chunk.js"
+  },
+  {
+    "revision": "d6b2fe0d96a8b8e5f008",
+    "url": "/static/js/88.aca998c0.chunk.js"
+  },
+  {
+    "revision": "ebe439b5d39150c5a4d6",
+    "url": "/static/js/89.9bc8c7d0.chunk.js"
+  },
+  {
+    "revision": "80b5ddedfb8aca7e9494",
+    "url": "/static/js/90.0edfd31b.chunk.js"
+  },
+  {
+    "revision": "8541841581555960dc5c",
+    "url": "/static/js/91.87b66151.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/static/js/91.87b66151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "162f91aceacb495580a1",
+    "url": "/static/js/92.147a06e2.chunk.js"
+  },
+  {
+    "revision": "9f226bf144f34e10dd20",
+    "url": "/static/js/93.1361d567.chunk.js"
+  },
+  {
+    "revision": "08ae1128d752971d0109",
+    "url": "/static/js/94.5bbf1a5c.chunk.js"
+  },
+  {
+    "revision": "b2f5094317713de70917",
+    "url": "/static/js/95.b069a5f4.chunk.js"
+  },
+  {
+    "revision": "96d7dab23b4b0d2a1885",
+    "url": "/static/js/96.fdf81f11.chunk.js"
+  },
+  {
+    "revision": "b8419fb89e981ea7b6bf",
+    "url": "/static/js/97.8c483598.chunk.js"
+  },
+  {
+    "revision": "ccd019cfdfa82b56edaa",
+    "url": "/static/js/98.12ecf25f.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/static/js/98.12ecf25f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88ff4fb4e14f4241f45d",
+    "url": "/static/js/99.9b5b70a8.chunk.js"
+  },
+  {
+    "revision": "ee48ebdd846daebd75cd",
+    "url": "/static/js/main.7aedbc3e.chunk.js"
+  },
+  {
+    "revision": "83096a8a419bdfd1c8de",
+    "url": "/static/js/polyfills-css-shim.9eee6e8b.chunk.js"
+  },
+  {
+    "revision": "5d4b7e036586893da1e1",
+    "url": "/static/js/runtime-main.b255b3f9.js"
+  },
+  {
+    "revision": "53b901ed4dc1d5c75d67",
+    "url": "/static/js/stencil-polyfills-css-shim.b6a800b7.chunk.js"
+  },
+  {
+    "revision": "7b5dd6649b4ea7f82594",
+    "url": "/static/js/stencil-polyfills-dom.bc66ed99.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/static/js/stencil-polyfills-dom.bc66ed99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b5a5c689fcc154c4b9d14317e3fed38",
+    "url": "/static/media/icons_large_global.4b5a5c68.png"
+  },
+  {
+    "revision": "b044be4abbb9c4a70d0984147fadd2c8",
+    "url": "/static/media/icons_large_merit.b044be4a.png"
+  }
+]);
--- gcc_mobile/src/pages/HowToPlay.tsx
+++ gcc_mobile/src/pages/HowToPlay.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonGrid, IonList, IonItem, IonCol, IonRow,  IonLabel, IonListHeader, IonCardHeader, IonImg, IonTextarea, IonText } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonCol,
+  IonRow,
+  IonImg,
+  IonText,
+} from '@ionic/react';
 import './HowToPlay.css';
-import logo from '../CSBlue.png';
-import { grid } from 'ionicons/icons';
 import prizes from '../image/icons_large_merit.png';
 import questions from '../image/icons_large_question.png';
 import globe from '../image/brandnet/icons_large_global.png';
@@ -12,174 +20,161 @@ import calendar from '../image/icons_large_calendar.png';
 const HowToPlay: React.FC = () => {
   return (
     <IonPage>
-        <IonHeader>
+      <IonHeader>
         <IonToolbar>
-          <IonText className="title">How to play</IonText>
+          <IonTitle>How to play</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent class="ion-padding">
-      <IonGrid> 
-        <IonRow>
-          <IonCol >    
-            <p>Follow five simple steps to successfully complete
-                                the Coding Challenge:
-            </p>
-                <IonText>
-                    <div className="BulletPoints">
-                    1. Register with your Github login
-                    </div>
-                    <div className="Points">
-                    Sign up for the Coding Challenge, making sure to provide your Github username to be able to submit code. 
-                    You can register either as an individual or as a member of a three-person team.
-                    </div>
-                </IonText>
-                <br></br>
-                <IonText >
-                    <div className="BulletPoints">
-                    2. Solve questions in your own time
-                    </div>
-                    <div className="Points">
-                    Work on solutions to the questions using any of Java, Python, C# or JavaScript.
-                    </div>
-                </IonText>
-                <br></br>
-                <IonText>
-                    <div className="BulletPoints">
-                    3. Submit you code
-                    </div>
-                    <div className="Points">
-                    Push your code back to the Github repository, so that our automated testing system can evaluate how you've done.
-                    </div>
-                </IonText>
-                <br></br>
-                <IonText>
-                    <div className="BulletPoints">
-                    4. Check your score
-                    </div>
-                    <div className="Points">
-                    Your code is judged against everybody else's, and then your position on the leaderboard is determined. See how you've done and try to improve!
-                    </div>
-                </IonText>
-                <br></br>
-                <IonText>
-                        <div className="BulletPoints">
-                        5. Win prizes
-                        </div>
-                        <div className="Points">
-                        At the end of the competition, the top performing students will receive various prizes!
-                        </div>
-                </IonText>   
-             </IonCol>
-           </IonRow>
-        </IonGrid>
-        
-        <br/>
-        <br/>
-
-        <IonGrid className="GreyBar">
-            <IonRow>
-                <IonCol size="3">
-                    <IonGrid>
-                        <IonRow>
-                            <IonCol size='2'>
-                                <IonImg src={calendar} style={{'height': '50px', 'float': 'right'}} />
-                            </IonCol>
-                            <IonCol size="4" style={{'paddingLeft': '0'}}>
-                                <IonText className="GreyBarColumn"> 1 <br/> Week</IonText>
-                            </IonCol>
-                        </IonRow>
-                    </IonGrid>  
-                </IonCol>
-                <IonCol size="3">
-                    <IonGrid>
-                        <IonRow>
-                            <IonCol size='2'>
-                                <IonImg src={globe} style={{'height': '50px', 'float': 'right'}} />
-                            </IonCol>
-                            <IonCol size="4" style={{'paddingLeft': '0'}}>
-                                <IonText className="GreyBarColumn"> 6 <br/> Regions</IonText>
-                            </IonCol>
-                        </IonRow>
-                    </IonGrid>  
-                </IonCol>               
-                <IonCol size="3">
-                    <IonGrid>
-                        <IonRow>
-                            <IonCol size='2'>
-                                <IonImg src={questions} style={{'height': '50px', 'float': 'right'}} />
-                            </IonCol>
-                            <IonCol size="4" style={{'paddingLeft': '0'}}>
-                                <IonText className="GreyBarColumn"> 6 <br/> Questions</IonText>
-                            </IonCol>
-                        </IonRow>
-                    </IonGrid>  
-                </IonCol>               
-                <IonCol size="3">
-                    <IonGrid>
-                        <IonRow>
-                            <IonCol size='2'>
-                                <IonImg src={prizes} style={{'height': '50px', 'float': 'right'}} />
-                            </IonCol>
-                            <IonCol size="4" style={{'paddingLeft': '0'}}>
-                                <IonText className="GreyBarColumn"> 3 <br/> Prizes</IonText>
-                            </IonCol>
-                        </IonRow>
-                    </IonGrid>  
-                </IonCol>
-            </IonRow>
-            <IonRow>
-                <IonCol size="3">
-                The challenge will run for 1 week, from
-                        December 11th to December 18th.
-                </IonCol>
-                <IonCol size="3">
-                Competition is run across Hong Kong & Singapore, India, Poland,
-                        Switzerland, UK & Ireland, and US.
-                </IonCol>
-                <IonCol size="3">
-                Six original questions for you to answer using
-                        any of four popular programming languages.
-                </IonCol>
-                <IonCol size="3">
-                There are prizes for the best individual, best team (of up to three), as well as the best overall region.
-                </IonCol>
-            </IonRow>
+      <IonContent>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">How to play</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <p>
+                Follow five simple steps to successfully complete the Coding
+                Challenge:
+              </p>
+              <IonText>
+                <div className="BulletPoints">
+                  1. Register with your Github login
+                </div>
+                <div className="Points">
+                  Sign up for the Coding Challenge, making sure to provide your
+                  Github username to be able to submit code. You can register
+                  either as an individual or as a member of a three-person team.
+                </div>
+              </IonText>
+              <br></br>
+              <IonText>
+                <div className="BulletPoints">
+                  2. Solve questions in your own time
+                </div>
+                <div className="Points">
+                  Work on solutions to the questions using any of Java, Python,
+                  C# or JavaScript.
+                </div>
+              </IonText>
+              <br></br>
+              <IonText>
+                <div className="BulletPoints">3. Submit you code</div>
+                <div className="Points">
+                  Push your code back to the Github repository, so that our
+                  automated testing system can evaluate how you've done.
+                </div>
+              </IonText>
+              <br></br>
+              <IonText>
+                <div className="BulletPoints">4. Check your score</div>
+                <div className="Points">
+                  Your code is judged against everybody else's, and then your
+                  position on the leaderboard is determined. See how you've done
+                  and try to improve!
+                </div>
+              </IonText>
+              <br></br>
+              <IonText>
+                <div className="BulletPoints">5. Win prizes</div>
+                <div className="Points">
+                  At the end of the competition, the top performing students
+                  will receive various prizes!
+                </div>
+              </IonText>
+            </IonCol>
+          </IonRow>
         </IonGrid>
 
-        <br/>
-        <br/>
+        <br />
+        <br />
 
-        <IonText style={{'display':'block','font-weight':'600','font-size':'230%'}}>
-            Prizes
+        <div className="GreyBar">
+          <div>
+            <IonImg src={calendar} style={{ height: '50px' }} />
+            <div className="header"> 1 Week </div>
+            <div>
+              The challenge will run for 1 week, from December 11th to December
+              18th.
+            </div>
+          </div>
+          <div>
+            <IonImg src={globe} style={{ height: '50px' }} />
+            <div className="header">6 Regions</div>
+            <div>
+              Competition is run across Hong Kong & Singapore, India, Poland,
+              Switzerland, UK & Ireland, and US.
+            </div>
+          </div>
+          <div>
+            <IonImg src={questions} style={{ height: '50px' }} />
+            <div className="header">6 Questions</div>
+            <div>
+              Six original questions for you to answer using any of four popular
+              programming languages.
+            </div>
+          </div>
+          <div>
+            <IonImg src={prizes} style={{ height: '50px' }} />
+            <div className="header">3 Prizes</div>
+            <div>
+              There are prizes for the best individual, best team (of up to
+              three), as well as the best overall region.
+            </div>
+          </div>
+        </div>
+
+        <br />
+        <br />
+
+        <IonText
+          style={{ display: 'block', fontWeight: '600', fontSize: '230%' }}
+        >
+          Prizes
+        </IonText>
+        <IonText >
+            <h2 style={{paddingLeft:20 , backgroundColor: '#f1f2f2'}}> Global Prizes</h2>
+            <div style={{paddingLeft:20}}> Global Challenge Winner</div>
+            <br></br>
+            <div className='Prizes'>1.Macbook Pro</div>
+            <h2 style={{paddingLeft:20, backgroundColor: '#f1f2f2'}}>Regional Prizes</h2>
+            <div style={{paddingLeft:20}}>The top three coders from each region will win the following prizes:</div>
+            <br></br>
+                <div  className='Prizes' >1. iPhone</div>
+                <div className='Prizes'>2. Smartwatch</div>
+                <div className='Prizes'>3. Portable bluetooth speaker</div>
+        </IonText>
+
+        <br />
+        <br />
+        <br />
+        <IonText
+          style={{ display: 'block', fontWeight: '600', fontSize: '230%' }}
+        >
+          <div>About the Coding Challenge</div>
         </IonText>
         <IonText>
-            TBA
-        </IonText>
-        
-        <br/>
-        <br/>
-        <br/>
-        <IonText style={{'display':'block','font-weight':'600','font-size':'230%'}}>
-            <div >
-                About the Coding Challenge
-            </div>
-         </IonText>
-         <IonText>
-         <p>
-                The Global Coding Challenge is an online coding competition between TAs across the
-                globe. Over 1 week, between 11th Dec and 18th Dec, TAs will be able to attempt solutions to siz
-                coding problems. TAs can enter either individually or as a team of up to three. Solutions will be evaluated on execution time and scored proportionally against solutions
-                provided by other students. Student can improve their code as many times as they like during the
-                competition. At 10am GMT, 18th December 2019, the Leaderboards will lock and the Global Coding Champion
-                will be announced.</p>
+          <p style={{paddingLeft:20}}>
+            The Global Coding Challenge is an online coding competition between
+            TAs across the globe. Over 1 week, between 11th Dec and 18th Dec,
+            TAs will be able to attempt solutions to siz coding problems. TAs
+            can enter either individually or as a team of up to three. Solutions
+            will be evaluated on execution time and scored proportionally
+            against solutions provided by other students. Student can improve
+            their code as many times as they like during the competition. At
+            10am GMT, 18th December 2019, the Leaderboards will lock and the
+            Global Coding Champion will be announced.
+          </p>
 
-            <p>The competition has been entirely designed, built and run by our technology graduates and interns.
-            </p>
-         </IonText>
+          <p style={{paddingLeft:20}}>
+            The competition has been entirely designed, built and run by our
+            technology graduates and interns.
+          </p>
+        </IonText>
       </IonContent>
     </IonPage>
   );
 };
-
 
 export default HowToPlay;
